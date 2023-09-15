d208307815dc963a260790204343997e38e75cca
nick v <nvalente.electrics@gmail.com>
2023-09-15 14:21:01 +0100

    removed errors from 3rd party sources in Tailwind - no more techincal errors

--- TradesMin_App/src/Components/Footer.tsx
+++ TradesMin_App/src/Components/Footer.tsx
@@ -16,8 +16,8 @@ export const Footer = () => {
               viewBox="0 0 24 24"
             >
               <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z" 
-              fill-rule="evenodd"
-              clip-rule="evenodd"/>
+              fillRule="evenodd"
+              clipRule="evenodd"/>
             </svg>
           </a>
           <a
@@ -32,8 +32,8 @@ export const Footer = () => {
             >
               <path
                 d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
           </a>
@@ -46,12 +46,12 @@ export const Footer = () => {
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               className="w-5 h-5 hover:bg-pink-500 hover:text-white rounded-xl sm:h-7 sm:w-7"
             >
               <path
-                stroke-linecap="round"
+                strokeLinecap="round"
                 d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25"
               />
             </svg>
