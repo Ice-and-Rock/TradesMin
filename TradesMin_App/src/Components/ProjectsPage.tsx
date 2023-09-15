15d6bec572b262b17b74f2603e741e77b39820bc
nick v <nvalente.electrics@gmail.com>
2023-09-15 14:50:34 +0100

    Added back button and corrected Type issues for Project type

--- TradesMin_App/src/Components/ProjectsPage.tsx
+++ TradesMin_App/src/Components/ProjectsPage.tsx
@@ -4,6 +4,7 @@ import ProjectDetails from "./ProjectDetails";
 
 type Project = {
   ProjectName: string;
+  products: Product[];
 };
 
 // type Product = {
@@ -20,20 +21,29 @@ type Project = {
 
 
 const ProjectsPage: React.FC = () => {
-  const [selectedProjectData, setSelectedProjectData] = useState(null)
+
+  const [selectedProjectData, setSelectedProjectData] = useState<Project | null> (null)
+
   const handleProjectSelection = (project: Project) => {
     setSelectedProjectData(project)
   }
+  const handleBackToProjects = () => {
+    setSelectedProjectData(null)
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-blue-100 text-gray-800">
       <h1 className="text-2xl font-bold mb-4">Projects Page</h1>
 
       {/* Project List */}
+      {selectedProjectData ? (
+      <ProjectDetails project={selectedProjectData} />
+      ) : (
       <ul>
         {projectsData.map((project: Project) => (
           <li 
           key={project.ProjectName} 
-          className="bg-blue-200 p-4 m-4 rounded-xl" 
+          className="bg-blue-200 p-4 m-4 rounded-xl shadow" 
           onClick={() => handleProjectSelection(project)} 
           style={{ cursor: "pointer"}}>
             {project.ProjectName}
@@ -50,7 +60,8 @@ const ProjectsPage: React.FC = () => {
           </li>
         ))}
       </ul>
-      {selectedProjectData && <ProjectDetails project={selectedProjectData} />}
+      )}
+      {selectedProjectData && <button onClick={handleBackToProjects} className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 m-4 rounded-full">Back</button>}
     </div>
   );
 };
