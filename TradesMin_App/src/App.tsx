c39f6a687b3cf87515d76de9e4dba2df8bd56831
nick v <nvalente.electrics@gmail.com>
2023-10-05 15:54:30 +0100

    ran build command for netlify

--- TradesMin_App/src/App.tsx
+++ TradesMin_App/src/App.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
-
 import { Home } from "./components/Home";
 import { Footer } from "./components/Footer";
 import ProjectsPage from "./components/ProjectsPagesV2/ProjectsPage"
