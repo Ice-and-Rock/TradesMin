b33f2657d4ae021a71c2fd9f307f577447541f46
nick v <nvalente.electrics@gmail.com>
2023-12-15 22:06:10 +0100

    added to About component - more descriptive

--- TradesMin_App/src/Components/About.tsx
+++ TradesMin_App/src/Components/About.tsx
@@ -3,12 +3,12 @@
 
 // export const About = () => {
 //   return (
-//     <Card className="mx-auto my-8 p-8 w-96 bg-blue-100 shadow-md rounded-md">
+//     <Card className="mx-auto  -8 p-8 w-96 bg-blue-100 shadow-md rounded-md">
 //       <Card.Title className="text-2xl font-bold mb-4 text-pink-600">
 //         Welcome new user!
 //       </Card.Title>
 //       <Card.Body>
-//         <h2 className="text-xl font-semibold mb-4">My name is Nick,</h2>
+//         <h2 className="text-xl font-semibold mb-4">  name is Nick,</h2>
 //         <p className="text-gray-800 mb-4">
 //           I built this project using React, TypeScript, Tailwind CSS, Bootstrap,
 //           and various other open-source libraries.
@@ -32,25 +32,37 @@ import logo from "../assets/tradesminLogo_v2.png";
 export const About = () => {
   return (
     <div className="flex items-center justify-center h-">
-      <Card style={{ width: "18rem", padding: "20px" }}>
+      <Card style={{ width: "20rem", padding: "10px" }}>
         <Card.Img variant="top" src={logo} />
         <Card.Body>
-          <Card.Title>Welcome new user!</Card.Title>
-          <Card.Title>I'm Nick Valente</Card.Title>
+          <Card.Text>
+            This app is designed to offer a practical solution to managing a small company in the Electrical industry. 
+          </Card.Text>
+          <Card.Text>
+            You can handle your Projects, Clients and Materials requirements using our secure databases and intuitive UI. 
+          </Card.Text>
+          <Card.Text>
+            Please feel free to Login and leave us some feedback. We imporove our systems based on our customers reviews.
+          </Card.Text>
+          <Card.Title>My name is Nick,</Card.Title>
           <Card.Text>
             I built this project using React, TypeScript, Tailwind CSS,
             Bootstrap, and various other open-source libraries.
           </Card.Text>
         </Card.Body>
+        <Card.Body>
+          
+          <Card.Text>Check out my other projects and feel free to contact me below. Thanks</Card.Text>
+        </Card.Body>
         <ListGroup className="list-group-flush">
           <ListGroup.Item>
-            <a href="https://github.com/Ice-and-Rock">My Github</a>{" "}
+            <a href="https://github.com/Ice-and-Rock">Github</a>{" "}
           </ListGroup.Item>
           <ListGroup.Item>
-            <a href="https://github.com/Ice-and-Rock">My LinkedIn</a>{" "}
+            <a href="https://github.com/Ice-and-Rock">LinkedIn</a>{" "}
           </ListGroup.Item>
           <ListGroup.Item>
-            <a href="https://github.com/Ice-and-Rock">My email</a>{" "}
+            <a href="https://github.com/Ice-and-Rock">email</a>{" "}
           </ListGroup.Item>
         </ListGroup>
         <Card.Body>
